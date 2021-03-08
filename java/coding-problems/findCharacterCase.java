00d1ec990deb09dfdc4ffb918aee370ab59d3418
Reet Batra <51335174+reetbatra@users.noreply.github.com>
2021-03-08 11:38:13 +0530

    Add files via upload

---
+++ java/coding-problems/findCharacterCase.java
@@ -0,0 +1,22 @@
+package codingques;
+import java.util.Scanner;
+public class findCharacterCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	       Scanner s = new Scanner(System.in);
+	        
+	        char ch = s.next().charAt(0);
+	        
+	        if(ch >= 'A' && ch <= 'Z') {
+	            System.out.println(1);
+	        }
+	        else if(ch >= 'a' && ch <= 'z') {
+	            System.out.println(0);
+	        }
+	        else {
+	            System.out.println(-1);
+	        }
+	    }
+	}
+
