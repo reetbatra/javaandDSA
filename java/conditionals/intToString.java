9b0088a3811d7776bb21dd6ef311dd416fb9059c
Reet Batra <51335174+reetbatra@users.noreply.github.com>
2021-03-08 10:45:20 +0530

    Add files via upload

---
+++ java/conditionals/intToString.java
@@ -0,0 +1,30 @@
+package ifelse;
+
+import java.util.Scanner;
+
+public class intToString {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+//		if (a==1) {
+//			System.out.println("One");
+//			
+//		}else if(a==2) {
+//			System.out.println("Two");
+//		}else if(a==3) {
+//			System.out.println("Three");
+//		}
+if (a==1) {
+	System.out.println("One");
+	
+}
+if(a==2) {
+	System.out.println("two");
+	
+}if(a==3) {
+	System.out.println("three");
+}
+	}
+
+}
