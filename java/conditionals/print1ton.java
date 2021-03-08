4007f56c9561537d5068fce98ee1241280de1b88
Reet Batra <51335174+reetbatra@users.noreply.github.com>
2021-03-08 11:24:21 +0530

    Add files via upload

---
+++ java/conditionals/print1ton.java
@@ -0,0 +1,16 @@
+package whileloops;
+import java.util.Scanner;
+public class print1ton {
+
+	public static void main(String[] args) {
+
+       Scanner s = new Scanner(System.in);
+       int n = s.nextInt();
+       int i = 1;
+       while(i<=n) {
+       System.out.println(i);
+       i= i+1;
+	}
+
+	}
+}
