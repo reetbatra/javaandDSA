6950e638c30bc36d71f78c151795fb5fdd8d408f
Reet Batra <51335174+reetbatra@users.noreply.github.com>
2021-03-12 00:01:45 +0530

    Add files via upload

---
+++ java/patterns/sqPattern.java
@@ -0,0 +1,21 @@
+package codingques;
+import java.util.Scanner;
+public class sqPattern {
+     public static void main(String[] args) {
+	 Scanner s = new Scanner(System.in);
+	 int n = s.nextInt();
+	 int i=1;
+	 while(i<=n) {
+		 int j=1;
+		 while(j<=n) {
+			 System.out.print("4");
+			 j++;
+		 }
+		 i++;
+		 System.out.println();
+	 }
+	 
+
+	}
+
+}
