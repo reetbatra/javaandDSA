0a20760d7cd01aee9ca1f0dbaf2de15b19a84318
Reet Batra <51335174+reetbatra@users.noreply.github.com>
2021-05-26 19:21:59 +0530

    Add files via upload

---
+++ java/searching-sorting/insertionsort.java
@@ -0,0 +1,25 @@
+
+public class insertionsort {
+    public static void insertionSort(int arr[]) {
+    	for(int i=1; i<arr.length; i++) {
+    	int	j= i-1;
+    	int temp = arr[i];
+    	while(j>=0 && arr[j] > temp) {
+    		arr[j+1] = arr[j];
+    		j--;
+    	}
+    	arr[j+1] = temp;
+    	
+    	}
+    	
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          int arr[] = {6,4,5,2,9};
+          insertionSort(arr);
+          for(int i=0; i< arr.length; i++) {
+        	  System.out.print(arr[i] + " ");
+          }
+	}
+
+}
