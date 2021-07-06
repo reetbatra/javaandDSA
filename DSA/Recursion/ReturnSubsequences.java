829b9e6d9176f162f80373e26f94b048df8d2d2a
Reet Batra <51335174+reetbatra@users.noreply.github.com>
2021-07-06 08:27:06 +0530

    Add files via upload

---
+++ DSA/Recursion/ReturnSubsequences.java
@@ -0,0 +1,37 @@
+package factorial;
+
+public class ReturnSubsequences {
+
+	public static String[] findSubsequences(String str) {
+		if(str.length() == 0) {
+			String ans[] = {""};
+			return ans;
+		}
+		String smallAns[] = findSubsequences(str.substring(1));
+		String ans[] = new String[2 * smallAns.length];
+		
+		//for copying
+		int k =0;
+		for(int i=0; i < smallAns.length; i++) {
+			ans[k] = smallAns[i];
+			k++;
+		}
+		//add first element to small ans
+		for(int i=0; i< smallAns.length; i++) {
+			//ans[i+ smallAns.length]
+			ans[k] = str.charAt(0) + smallAns[i];
+			 k++;
+		}
+		return ans;
+	}
+	
+	public static void main(String[] args) {
+		String str = "xyz";
+		String ans[] = findSubsequences(str);
+		for(int i = 0; i<ans.length; i++) {
+			System.out.println(ans[i]);
+		}
+
+	}
+
+}
