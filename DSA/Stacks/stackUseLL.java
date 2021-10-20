fbfb7303f07129b8a4d32d400bc1205273cd473e
Reet Batra <51335174+reetbatra@users.noreply.github.com>
2021-10-20 15:42:48 +0530

    Create stackUseLL.java

---
+++ DSA/Stacks/stackUseLL.java
@@ -0,0 +1,13 @@
+package Stacks;
+
+public class stackUse {
+
+	public static void main(String[] args) throws stackFullException {
+		
+		//creating an object for stack using LL
+		
+		stackUsingLL<Integer> stack = new stackUsingLL<>();
+
+	}
+
+}
